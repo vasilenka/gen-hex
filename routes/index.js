43ef7697580bff5f0f5670758a19411e51b440e1
vasilenka <meepomodeon@gmail.com>
2019-02-03 12:19:39 +0700

    now using colorThief and Chroma.js, awesome! 🔥

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const sharp = require('sharp');
 const fs = require('fs');
 const request = require('request');
 const getColors = require('get-image-colors');
+const { getColorFromURL } = require('color-thief-node');
+const chroma = require('chroma-js');
 
 const download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
@@ -18,22 +20,28 @@ router.post('/', function(req, res, next) {
   const values = Object.values(req.files);
 
   values.map(img => {
-    console.log('img: ', img)
     // writeFile( img, 'file.jpg', (name) => {
     // download( img, 'processing.jpg', (name) => {
       // sharp(`./${name}`)
       sharp(img.path)
-        .resize(1, 1)
+        // .resize(1, 1)
         .toFormat('jpg')
         .toBuffer()
-        .then(data => {
-          getColors(data, 'image/jpeg')
-          .then(colors => {
-            colors.map(color => res.send({color: color.hex()}))
-          })
-          .catch(err => {
-            console.log(err)
-          })
+        .then(async data => {
+
+          const dominantColor = await getColorFromURL(data);
+          res.send({colors: chroma(`rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`).hex()})
+
+          // getColors(data, 'image/jpeg')
+          //   .then(colors => {
+          //     let hexColors = colors.map(color => color.hex())
+          //     console.log(hexColors)
+          //     res.send({colors: hexColors})
+          //   })
+          //   .catch(err => {
+          //     console.log(err)
+          //   })
+
         })
         .catch(err => {
           console.log(err)
