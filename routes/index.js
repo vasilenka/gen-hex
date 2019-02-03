39725139bdc48bd42649ba9d5dd38934dcb0e627
vasilenka <meepomodeon@gmail.com>
2019-02-03 13:07:44 +0700

    comment all unnecessary code to avoid confusion

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const sharp = require('sharp');
-const fs = require('fs');
-const request = require('request');
-const getColors = require('get-image-colors');
+// const fs = require('fs');
+// const request = require('request');
+// const getColors = require('get-image-colors');
 const { getColorFromURL } = require('color-thief-node');
 const chroma = require('chroma-js');
 
-const download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', () => callback(filename));
-  });
-};
+// const download = function(uri, filename, callback){
+//   request.head(uri, function(err, res, body){
+//     console.log('content-type:', res.headers['content-type']);
+//     console.log('content-length:', res.headers['content-length']);
+//     request(uri).pipe(fs.createWriteStream(filename)).on('close', () => callback(filename));
+//   });
+// };
 
 router.post('/', function(req, res, next) {
 
@@ -35,7 +35,6 @@ router.post('/', function(req, res, next) {
           // getColors(data, 'image/jpeg')
           //   .then(colors => {
           //     let hexColors = colors.map(color => color.hex())
-          //     console.log(hexColors)
           //     res.send({colors: hexColors})
           //   })
           //   .catch(err => {
